0abbbba3eaa94f3cd6ef234943801818afb00723
shubham oulkar <oulkarshubhu@gmail.com>
2024-05-24 18:33:37 +0530

    initial files

---
+++ Tic-Tac-Toe/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+
+  function handleClick(squ) {
+    const nextBoard = square.slice();
+    nextBoard[squ] = "X";
+    setSquare(nextBoard);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+export default Board;

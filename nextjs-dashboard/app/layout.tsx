25f7096621bb4885b81ba63d7a742840713988a3
shubham oulkar <oulkarshubhu@gmail.com>
2024-06-11 09:56:20 +0530

    vercel web vitals

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/font';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -20,7 +21,7 @@ export const metadata: Metadata = {
     address: false,
     telephone: false,
   },
-  metadataBase: new URL('https://financial-dashboard-shubhu-oulkar.vercel.app'),
+  metadataBase: new URL('https://financialdashboard-beige.vercel.app/'),
   alternates: {
     canonical: '/',
     languages: {
@@ -31,16 +32,16 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Next.js',
     description: 'The React Framework for the Web',
-    url: 'https://financial-dashboard-shubhu-oulkar.vercel.app',
+    url: 'https://financialdashboard-beige.vercel.app/',
     siteName: 'Next.js',
     images: [
       {
-        url: 'https://financial-dashboard-shubhu-oulkar.vercel.app/og.png', // Must be an absolute URL
+        url: 'https://financialdashboard-beige.vercel.app/og.png', // Must be an absolute URL
         width: 800,
         height: 600,
       },
       {
-        url: 'https://financial-dashboard-shubhu-oulkar.vercel.app/og-alt.png', // Must be an absolute URL
+        url: 'https://financialdashboard-beige.vercel.app/og-alt.png', // Must be an absolute URL
         width: 1800,
         height: 1600,
         alt: 'My custom alt',
@@ -51,47 +52,13 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Next.js',
-    description: 'The React Framework for the Web',
+    title: 'The Financial Dashboard',
+    description: 'The Financial Dashboard, built with nextjs App Router.',
     siteId: '1467726470533754880',
-    creator: '@nextjs',
+    creator: '@shubham',
     creatorId: '1467726470533754880',
-    images: ['https://financial-dashboard-shubhu-oulkar.vercel.app/og.png'], // Must be an absolute URL
+    images: ['https://financialdashboard-beige.vercel.app/og.png'], // Must be an absolute URL
   },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  // icons: {
-  //   icon: [
-  //     { url: '/icon.png' },
-  //     new URL(
-  //       '/icon.png',
-  //       'https://financial-dashboard-shubhu-oulkar.vercel.app',
-  //     ),
-  //     { url: '/icon-dark.png', media: '(prefers-color-scheme: dark)' },
-  //   ],
-  //   shortcut: ['/shortcut-icon.png'],
-  //   apple: [
-  //     { url: '/apple-icon.png' },
-  //     { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
-  //   ],
-  //   other: [
-  //     {
-  //       rel: 'apple-touch-icon-precomposed',
-  //       url: '/apple-touch-icon-precomposed.png',
-  //     },
-  //   ],
-  // },
 };
 
 export default function RootLayout({
@@ -105,6 +72,9 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </head>
       <body className={`${inter.className} antialised`}>{children}</body>
+      {/* No vercel web vitals send to the server during development. 
+          Hydration Error is there during development. See console for more info. */}
+      <SpeedInsights />
     </html>
   );
 }

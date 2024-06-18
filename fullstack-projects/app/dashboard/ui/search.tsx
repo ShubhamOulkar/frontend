6cb56e2a611b5e9ac8ae55f3b7426ff61d494851
shubham oulkar <oulkarshubhu@gmail.com>
2024-06-18 10:55:25 +0530

    remove console.log from search component

--- fullstack-projects/app/dashboard/ui/search.tsx
+++ fullstack-projects/app/dashboard/ui/search.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useRef } from 'react';
-import { useDebouncedCallback } from 'use-debounce';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useRef } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const inputRef = useRef<HTMLInputElement | null>(null);
@@ -12,17 +12,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   // console.log(searchParams.get('query')?.toString());
-  console.log('inputRef', inputRef);
 
   const handleIconClick = () => {
     if (inputRef.current) inputRef.current.focus();
   };
 
   const handleSearch = useDebouncedCallback((value: string) => {
-    console.log('searching....', value);
+    console.log("searching....", value);
     const params = new URLSearchParams(searchParams);
 
-    value ? params.set('query', value) : params.delete('query');
+    value ? params.set("query", value) : params.delete("query");
 
     replace(`${pathname}?${params.toString()}`);
   }, 1000);
@@ -38,7 +37,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon
         className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 cursor-pointer text-gray-500 peer-focus:text-gray-900"

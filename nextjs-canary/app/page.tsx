8d7073d6b8ffa874b09542de867e9756a61335d5
shubham oulkar <oulkarshubhu@gmail.com>
2024-06-21 09:03:59 +0530

    add stable and canary projects for nextjs

---
+++ nextjs-canary/app/page.tsx
@@ -0,0 +1,33 @@
+import callApi from "./lib/callApi";
+
+// export const dynamic = "force-dynamic";
+// export const fetchCache = "default-cache";
+// export const fetchCache = "force-no-cache";
+// export const fetchCache = "no-store";
+// export const revalidate = 10;
+// export const runtime = "nodejs";
+// 'nodejs' | 'edge'
+
+export default async function Home() {
+  const data = callApi();
+
+  console.log("Data fetched from page");
+
+  return (
+    <div>
+      <h2>
+        <strong>Next 14 </strong>
+        In page.js, fetch() data cached by default (server component), to fetch
+        data on each request use cache: &apos;no-store&apos;:
+      </h2>
+      <q>{data}</q>
+
+      <div>
+        <strong>
+          Next 15 : In new release fetch() is not cached by default in
+          development. But in production data is cached by default.
+        </strong>
+      </div>
+    </div>
+  );
+}
---
+++ nextjs-stable/app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import callApi from "./lib/callApi";
+
+const inter = Inter({ subsets: ["latin"] });
+
+// export const fetchCache = "force-no-store";
+
+// export const revalidate = 10;
+
+export const metadata: Metadata = {
+  title: {
+    template: '"%s | Learn nextjs',
+    default: "Learn app routes in nextjs",
+  },
+  description: "Learn app routes in nextjs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const data = callApi();
+
+  console.log("Data fetched from layout");
+
+  // const cookie = headers();
+  // console.log("cookie:", cookie);
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col items-center justify-start p-24">
+          <div>
+            <h1>This is home page</h1>
+            <div>
+              <h2>
+                <strong>Next 14 </strong>
+                In layout.js, fetch() data is not cached by default (server
+                component), to stop fetch data on each request use cache:
+                &apos;force-cache&apos;:
+              </h2>
+              <q>{data}</q>
+            </div>
+          </div>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
